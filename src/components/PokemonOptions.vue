f33ad27713cdd8f9b6829f9309995739a6e51153
mauro-vargas-gl <mauro.vargas@globant.com>
2021-10-09 10:32:30 -0300

    options are displayed

--- src/components/PokemonOptions.vue
+++ src/components/PokemonOptions.vue
@@ -1,16 +1,22 @@
 <template>
     <div class="options-container">
         <ul>
-            <li>1</li>
-            <li>2</li>
-            <li>3</li>
-            <li>4</li>
+            <li v-for="pokemon in pokemons" :key="pokemon.id">
+                {{ pokemon.name }}
+            </li>
         </ul>
     </div>
 </template>
 
 <script>
-export default {}
+export default {
+    props: {
+        pokemons: {
+            type: Array,
+            required: true,
+        },
+    },
+};
 </script>
 
 <style scoped>
