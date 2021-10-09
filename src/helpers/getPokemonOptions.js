bc41abe5602fa03e1d019d074fbdea84d1148319
mauro-vargas-gl <mauro.vargas@globant.com>
2021-10-09 08:53:31 -0300

    promise array of pokemons is ready

--- src/helpers/getPokemonOptions.js
+++ src/helpers/getPokemonOptions.js
@@ -1,13 +1,33 @@
+import pokApi from '../api/pokemonApi';
+
 const getPokemons = () => {
     const pokeArr = Array.from(Array(650));
     return pokeArr.map((_, index) => index + 1);
 };
 
-const getPokemonOptions = () => {
+const getPokemonOptions = async () => {
     const mixedPokemons = getPokemons().sort(() => Math.random() - 0.5);
-    getPokemonNames(mixedPokemons.splice(0, 4));
+    const pokemons = await getPokemonNames(mixedPokemons.splice(0, 4));
+
+    return pokemons;
 };
 
-const getPokemonNames = (pokemons = []) => {};
+const getPokemonNames = async ([a, b, c, d] = []) => {
+    const promiseArr = [
+        pokApi.get(`/${a}`),
+        pokApi.get(`/${b}`),
+        pokApi.get(`/${c}`),
+        pokApi.get(`/${d}`),
+    ];
+
+    const [p1, p2, p3, p4] = await Promise.all(promiseArr);
+
+    return [
+        { name: p1.data.name, id: p1.data.id },
+        { name: p2.data.name, id: p2.data.id },
+        { name: p3.data.name, id: p3.data.id },
+        { name: p4.data.name, id: p4.data.id },
+    ];
+};
 
 export default getPokemonOptions;
