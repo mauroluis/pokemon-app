fc5d8badfc908a2f7b85df169570a6b37364801f
mauro-vargas-gl <mauro.vargas@globant.com>
2021-10-09 10:32:30 -0300

    options are displayed

--- src/pages/PokemonPage.vue
+++ src/pages/PokemonPage.vue
@@ -3,14 +3,30 @@
 
     <PokemonImg :pokemonId="3" :showPokemon="true" />
 
-    <PokemonOptions />
+    <PokemonOptions :pokemons="pokemonArr" />
 </template>
 
 <script>
 import PokemonImg from '@/components/PokemonImg.vue';
 import PokemonOptions from '@/components/PokemonOptions.vue';
 import getPokemonOptions from '@/helpers/getPokemonOptions.js';
+
 export default {
     components: { PokemonImg, PokemonOptions },
+    //data siempre retorna un objeto
+    data() {
+        return {
+            pokemonArr: [],
+        };
+    },
+    methods: {
+        async mixPokemonArray() {
+            this.pokemonArr = await getPokemonOptions();
+        },
+    },
+    //llamando a mounted lifecycle hook (metodo que se llama 1 sola vez)
+    mounted() {
+        this.mixPokemonArray();
+    },
 };
 </script>
