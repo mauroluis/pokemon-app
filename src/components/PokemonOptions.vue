1844e1029877bdf8e0e64effa17cb84914eed136
mauro-vargas <mauro.vargas@globant.com>
2021-10-16 09:06:04 -0300

    pokemons now show and options can make you choose and shows a validation message

--- src/components/PokemonOptions.vue
+++ src/components/PokemonOptions.vue
@@ -1,7 +1,12 @@
 <template>
     <div class="options-container">
+        <!-- $emit es el metodo del objeto Vue que sirve para "emitir" valores. lleva un elemento obligatorio que es el nombre del "evento" a disparar y el segundo sirve para mandar datos (opcional) que en esta ocasion envía el ID del pokemon -->
         <ul>
-            <li v-for="pokemon in pokemons" :key="pokemon.id">
+            <li
+                v-for="pokemon in pokemons"
+                :key="pokemon.id"
+                @click="$emit('selectionPokemon', pokemon.id)"
+            >
                 {{ pokemon.name }}
             </li>
         </ul>
--- src/pages/PokemonPage.vue
+++ src/pages/PokemonPage.vue
@@ -1,9 +1,19 @@
 <template>
-    <h1>¿Quien es este Pokémon?</h1>
+    <h1 v-if="!pokemon">Cargando...</h1>
+    <div v-else>
+        <h1>¿Quien es este Pokémon?</h1>
 
-    <PokemonImg :pokemonId="3" :showPokemon="true" />
+        <PokemonImg :pokemon-id="pokemon.id" :show-pokemon="showPokemon" />
+        <!-- Cuando se haga click podemos "escucharlo" poniendo @ y el nombre que le dimos al metodo $emit() -->
+        <PokemonOptions
+            :pokemons="pokemonArr"
+            @selectionPokemon="checkAnswer"
+        />
 
-    <PokemonOptions :pokemons="pokemonArr" />
+        <h2>{{ message }}</h2>
+
+        <button>Nuevo Juego</button>
+    </div>
 </template>
 
 <script>
@@ -17,14 +27,29 @@ export default {
     data() {
         return {
             pokemonArr: [],
+            pokemon: null,
+            showPokemon: false,
+            showAnswer: false,
+            message: '',
         };
     },
     methods: {
         async mixPokemonArray() {
             this.pokemonArr = await getPokemonOptions();
+
+            const rndInt = Math.floor(Math.random() * 4);
+            //mandamos el rndInt como prop al array asi pokemon es cargado con alguno de los elementos del array
+            this.pokemon = this.pokemonArr[rndInt];
+        },
+        checkAnswer(selectedId) {
+            this.showPokemon = true;
+            this.showAnswer = false;
+            selectedId === this.pokemon.id
+                ? (this.message = `Exacto! El pokemon es: ${this.pokemon.name}`)
+                : (this.message = `Lo siento, el pokemon es ${this.pokemon.name}`);
         },
     },
-    //llamando a mounted lifecycle hook (metodo que se llama 1 sola vez)
+    //llamando a mounted lifecycle hook (metodo que se llama 1 sola vez en cada recarga)
     mounted() {
         this.mixPokemonArray();
     },
